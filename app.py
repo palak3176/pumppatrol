02bb97e49da061fe08cac7c7be781e9bcba0fc99
palak3176 <jainpalak3176@gmail.com>
2026-04-02 12:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,13 @@ import random
 app = Flask(__name__)
 
 # --- DATABASE CONFIG ---
-db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '31072006Palak',  # <--- UPDATE THIS
-    'database': 'pumppatrol'
+import os
+
+DB_CONFIG = {
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'database': os.getenv("DB_NAME")
 }
 
 
