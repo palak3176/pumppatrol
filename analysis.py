a62d0ded2a7cc83f964070989193b51a126c12ce
palak3176 <jainpalak3176@gmail.com>
2026-04-02 12:28:37 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,8 +1,12 @@
 import mysql.connector
 
+import os
+
 DB_CONFIG = {
-    'host': '127.0.0.1', 'user': 'root',
-    'password': '31072006Palak', 'database': 'pumppatrol'
+    'host': os.getenv("DB_HOST"),
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'database': os.getenv("DB_NAME")
 }
 
 
